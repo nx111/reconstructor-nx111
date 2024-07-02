20b1663239002bb9b4afab8e1bd3e65c8563666f
nx111 <gd.zhangdz@gmail.com>
2024-07-02 17:39:28 +0800

    update version year to 4 digit numbers.

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,3 +1,3 @@
 version=$(cat trunk/DEBIAN/control | grep Version: | cut -d: -f2 | sed -e "s/^ \{1,\}//g" | cut -d- -f1)
-eval sed -e \"s/\\\(Version: *$version-\\\)[[:digit:]]*/\\1$(date +%y%m%d)/\" -i trunk/DEBIAN/control
+eval sed -e \"s/\\\(Version: *$version-\\\)[[:digit:]]*/\\1$(date +%Y%m%d)/\" -i trunk/DEBIAN/control
 dpkg -b trunk reconstructor-$version-nx111-$(date +%Y%m%d).deb
